318d366344f5ab6a0b66c399d56e1ccbb987eb98
Игорь <112966801+SmallUpdate@users.noreply.github.com>
2023-05-27 15:43:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,26 @@ window.addEventListener('scroll', function() {
         button_up.classList.remove('button_up_show');
     }
 });
+
+const hamburger = document.querySelector('.header__hamburger');
+const menu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
+const item = document.querySelectorAll('.header__menu__item');
+
+hamburger.addEventListener('mousedown', function() {
+    toggleMenu();
+});
+
+for (var i = 0; i < item.length; i++) {
+    item[i].addEventListener('click', function() {
+        toggleMenu();
+    });
+}
+
+function toggleMenu() {
+    hamburger.classList.toggle('header__hamburger_active');
+    menu.classList.toggle('header__menu_show');
+    body.classList.toggle('body_overflow');
+}
+
+document.querySelector('body').classList.add('loaded');
